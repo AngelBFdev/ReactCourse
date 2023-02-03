864b4935d9452e0854318d8e367f08ed3f0843ac
AngelFBI <angel.bejarano@enroutesystems.com>
2023-02-02 22:41:20 -0600

    Assigment

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,18 +2,18 @@ import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-const ExpenseItem=({date, title, amount}) =>{
+const ExpenseItem = ({ date, title, amount }) => {
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={date}/>
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">
-          ${amount}
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">${amount}</div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
