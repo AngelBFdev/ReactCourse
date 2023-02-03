6344fcb6c7cb7db3a2c6876bd1424782d7bf80e1
AngelFBI <angel.bejarano@enroutesystems.com>
2023-02-02 21:57:32 -0600

    Assigment

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,30 +11,22 @@ const Expenses=({items}) =>{
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  })
+
   return (
     <div>
     <Card className="expenses">
-      <ExpensesFilter onChangeFilter={filterChangeHandler} />
-      <ExpenseItem
-        title={items[0].title}
-        amount={items[0].amount}
-        date={items[0].date}
-      />
-      <ExpenseItem
-        title={items[1].title}
-        amount={items[1].amount}
-        date={items[1].date}
-      />
-      <ExpenseItem
-        title={items[2].title}
-        amount={items[2].amount}
-        date={items[2].date}
-      />
-      <ExpenseItem
-        title={items[3].title}
-        amount={items[3].amount}
-        date={items[3].date}
-      />
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      {filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
     </div>
   );
